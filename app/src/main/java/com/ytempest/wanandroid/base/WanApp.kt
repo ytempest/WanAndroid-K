095a75ade689ad5bc1719398f57d543e47aa9530
ytempest <787491096@qq.com>
2022-02-10 18:27:54 +0800

    移除dagger2

--- app/src/main/java/com/ytempest/wanandroid/base/WanApp.kt
+++ app/src/main/java/com/ytempest/wanandroid/base/WanApp.kt
@@ -1,21 +1,14 @@
 package com.ytempest.wanandroid.base
 
-import android.app.Activity
 import android.app.Application
 import com.ytempest.tool.ToolModule
 import com.ytempest.tool.util.LogUtils
-import com.ytempest.wanandroid.di.component.AppComponent
-import com.ytempest.wanandroid.di.component.DaggerAppComponent
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import javax.inject.Inject
 
 /**
  * @author heqidu
  * @since 21-2-7
  */
-class WanApp : Application(), HasActivityInjector {
+class WanApp : Application() {
 
     companion object {
         lateinit var instance: WanApp
@@ -28,15 +21,5 @@ class WanApp : Application(), HasActivityInjector {
         ToolModule.init(this)
         LogUtils.setLogPrefix("[WanAndroid]: ")
         LogUtils.setLoggable(true)
-
-        val appComponent: AppComponent = DaggerAppComponent.builder().build()
-        appComponent.inject(this)
-    }
-
-    @Inject
-    lateinit var mAndroidInjector: DispatchingAndroidInjector<Activity>
-
-    override fun activityInjector(): AndroidInjector<Activity> {
-        return mAndroidInjector
     }
 }
--- app/src/main/java/com/ytempest/wanandroid/interactor/impl/HttpHelperImpl.kt
+++ app/src/main/java/com/ytempest/wanandroid/interactor/impl/HttpHelperImpl.kt
@@ -17,48 +17,59 @@ class HttpHelperImpl : HttpHelper {
     private val mHttpApi: HttpApi = HttpModule.instance.getHttpApi()
 
     override fun getHomeArticleList(pageNum: Int): Call<BaseResp<HomeArticleBean>> =
-            mHttpApi.getHomeArticleList(pageNum)
+        mHttpApi.getHomeArticleList(pageNum)
 
     override fun login(account: String, password: String): Call<BaseResp<LoginBean>> =
-            mHttpApi.login(account, password)
+        mHttpApi.login(account, password)
 
 
-    override fun register(account: String?, pwd: String?, confirmPwd: String?): Call<BaseResp<LoginBean>> =
-            mHttpApi.register(account, pwd, confirmPwd)
+    override fun register(
+        account: String?,
+        pwd: String?,
+        confirmPwd: String?
+    ): Call<BaseResp<LoginBean>> =
+        mHttpApi.register(account, pwd, confirmPwd)
 
     override fun getBannerList(): Call<BaseResp<List<BannerBean>>> =
-            mHttpApi.getBannerList()
+        mHttpApi.getBannerList()
 
     override fun getMyCollectionList(): Observable<BaseResp<MyCollectionBean>> =
-            mHttpApi.getMyCollectionList();
+        mHttpApi.getMyCollectionList();
 
     override fun addCollectArticle(articleId: Long): Call<BaseResp<ArticleCollectBean>> =
-            mHttpApi.addCollectArticle(articleId);
+        mHttpApi.addCollectArticle(articleId);
 
 
-    override fun addCollectOutsideArticle(title: String?, author: String?, link: String?): Call<BaseResp<OutsideArticleCollectBean>> {
+    override fun addCollectOutsideArticle(
+        title: String?,
+        author: String?,
+        link: String?
+    ): Call<BaseResp<OutsideArticleCollectBean>> {
         return mHttpApi.addCollectOutsideArticle(title, author, link)
     }
 
     override fun cancelCollectArticle(articleId: Long): Call<BaseResp<ArticleCollectBean>> =
-            mHttpApi.cancelCollectArticle(articleId)
+        mHttpApi.cancelCollectArticle(articleId)
 
     override fun cancelMyCollectArticle(articleId: Long): Observable<BaseResp<ArticleCollectBean>> =
-            mHttpApi.cancelMyCollectArticle(articleId)
+        mHttpApi.cancelMyCollectArticle(articleId)
 
     override fun getKnowledgeArchitecture(): Call<BaseResp<List<KnowledgeArchitectureBean>>> =
-            mHttpApi.getKnowledgeArchitecture()
+        mHttpApi.getKnowledgeArchitecture()
 
-    override fun getArchitectureContent(id: Int, page: Int): Call<BaseResp<ArchitectureContentBean>> {
+    override fun getArchitectureContent(
+        id: Int,
+        page: Int
+    ): Call<BaseResp<ArchitectureContentBean>> {
         return mHttpApi.getArchitectureContent(page, id)
     }
 
     override fun getNavigationList(): Call<BaseResp<List<NavigationListBean>>> =
-            mHttpApi.getNavigationList()
+        mHttpApi.getNavigationList()
 
     override fun getProjectClassify(): Call<BaseResp<List<ProjectClassifyBean>>> =
-            mHttpApi.getProjectClassify()
+        mHttpApi.getProjectClassify()
 
     override fun getProjectContent(page: Int, cid: Int): Call<BaseResp<ProjectContentBean>> =
-            mHttpApi.getProjectContent(page, cid)
+        mHttpApi.getProjectContent(page, cid)
 }
