1f6eae4022f5a18e6df3899b23c87b79171714aa
ytempest <787491096@qq.com>
2025-05-10 21:01:29 +0800

    widget控件本地化

---
+++ app/src/main/java/com/ytempest/variety/tab/ScaleTabDecorator.java
@@ -0,0 +1,59 @@
+package com.ytempest.variety.tab;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ytempest.variety.TabGroup;
+import com.ytempest.wanandroid.R;
+
+
+/**
+ * @author heqidu
+ * @since 2021/1/12
+ */
+public class ScaleTabDecorator implements ITabDecorator<String> {
+
+    @Override
+    public final View createTabView(LayoutInflater inflater, TabGroup parent, int position) {
+        View view = onCreateTabView(inflater, parent, position);
+        float scale = getScaleRatio();
+        view.setScaleX(scale);
+        view.setScaleY(scale);
+        return view;
+    }
+
+    protected float getScaleRatio() {
+        return 0.85F;
+    }
+
+    protected View onCreateTabView(LayoutInflater inflater, TabGroup parent, int position) {
+        return inflater.inflate(R.layout.tab_scale_text_view, parent, false);
+    }
+
+    @Override
+    public void onBindTabView(View tab, String text, int position) {
+        TextView view = (TextView) tab;
+        view.setText(text);
+    }
+
+    @Override
+    public void onCurrentTabOffset(View tab, float offsetPercent) {
+        final float ratio = getScaleRatio();
+        final float scale = ratio - (ratio - 1) * (1 - offsetPercent);
+        tab.setPivotX(tab.getWidth() / 2F);
+        tab.setPivotY(tab.getHeight() / 2F);
+        tab.setScaleX(scale);
+        tab.setScaleY(scale);
+    }
+
+    @Override
+    public void onNextTabOffset(View tab, float offsetPercent) {
+        final float ratio = getScaleRatio();
+        final float scale = ratio - (ratio - 1) * offsetPercent;
+        tab.setPivotX(tab.getWidth() / 2F);
+        tab.setPivotY(tab.getHeight() / 2F);
+        tab.setScaleX(scale);
+        tab.setScaleY(scale);
+    }
+}
